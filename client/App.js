370497b3bb713baefdc60f3e9920ce23dda50189
Anshuman Sinha <anshuman@Anshumans-MacBook-Air.local>
2022-09-08 15:56:27 -0500

    add proprietary light/dark mode toggle

--- client/App.js
+++ client/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import { hot } from 'react-hot-loader/root';
 import LandingPage from  './Containers/LandingPage';
 import SignupPage from "./Containers/SignupPage";
@@ -7,15 +7,26 @@ import MusicContainer from './Containers/musicRec';
 import LoadingContainer from './Containers/LoadingContainer';
 import ProfileComponent from './Containers/ProfileContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./styles.css";
 
 
 function App() {
-  const [username, setUsername] = useState('')
+  const storedDarkMode = localStorage.getItem("DARK_MODE");
+  const [darkMode, setDarkMode] = useState(storedDarkMode);
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+  useEffect(() => {
+    localStorage.setItem("DARK_MODE", darkMode);
+  }, [darkMode]);
 
+  const [username, setUsername] = useState('')
     return (
       <React.Fragment>
         <BrowserRouter>
-          <Routes>
+        {/* <div className="App" data-theme={darkMode ? "dark" : "light"}>
+          Hello
+        <button onClick={toggleDarkMode}>Toggle</button>
+        </div> */}
+          <Routes data-theme={darkMode ? "dark" : "light"}>
             <Route path='/' element={<LandingPage username={username} setUsername={setUsername}/>} />
             <Route path='/signup' element={<SignupPage />} />
             <Route path='/profile' element={<ProfileComponent />} />
